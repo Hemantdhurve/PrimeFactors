05f8f416994be75af7bce10a550717e550553ea3
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-30 10:22:15 +0530

    [Hemant] Added Prime Factors Program

---
+++ PrimeFactors/Factors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    public class Factors
+    {
+        public void PFactors()
+        {
+            Console.WriteLine("\tEnter the Number which User want to get Prime Factors:");
+            Console.WriteLine("Enter Number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 2; i < number; i++)
+            {
+                while (number % i == 0)
+                {
+                    Console.WriteLine(i);
+
+                    number = number / i;
+                }
+            }
+
+            if(number > 2)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ PrimeFactors/Program.cs
@@ -0,0 +1,17 @@
+﻿using PrimeFactors;
+using System;
+
+namespace Primefactors
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Prime Factors Program");
+           
+
+            Factors prime=new Factors();
+            prime.PFactors();
+        }
+    }
+}
